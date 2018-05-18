b6df77b2e27018d871af9202d4eeeee687423ce2
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2018-05-18 20:03:37 +0300

    initial configuration of the docker-based config

---
+++ docker-entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+
+COMMANDS="debug help logtail show stop adduser fg kill quit run wait console foreground logreopen reload shell status"
+START="start restart"
+
+#create the Maildir folder structure
+mkdir -p /var/local/privacyforum/var/naaya-mail-queue/new
+mkdir -p /var/local/privacyforum/var/naaya-mail-queue/cur
+mkdir -p /var/local/privacyforum/var/naaya-mail-queue/tmp
+chown 500:500 -R /var/local/privacyforum/var/naaya-mail-queue
+
+if [ ! -z "$CRONTAB" ]; then
+  sed -i -e "s/LINKCHECKER_USER/$LINKCHECKER_USER/g" crontab.cfg
+  sed -i -e "s/LINKCHECKER_PASSWORD/$LINKCHECKER_PASSWORD/g" crontab.cfg
+  crontab -u zope crontab.cfg
+  crond
+fi
+
+if [[ $START == *"$1"* ]]; then
+  _stop() {
+    bin/instance stop
+    kill -TERM $child 2>/dev/null
+  }
+
+  /usr/sbin/crond
+  trap _stop SIGTERM SIGINT
+
+  if [ ! -z "$DEBUG_MODE" ]; then
+    bin/develop up
+  fi
+
+  bin/instance start
+  bin/instance logtail &
+
+  child=$!
+  wait "$child"
+else
+  if [[ $COMMANDS == *"$1"* ]]; then
+    exec bin/instance "$@"
+  fi
+  exec "$@"
+fi
